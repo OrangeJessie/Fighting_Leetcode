5a0cda10ffc3f0b660331db12e4d7228aae7771e
Jessie <976448731@qq.com>
2019-01-02 10:42:56 +0800

    Add files via upload

--- 初级算法/palindromeList.py
+++ 初级算法/palindromeList.py
@@ -4,8 +4,15 @@ class ListNode:
         self.val = x
         self.next = None
 
+    def addList(self, head, b):
+        node = head
+        for i in b:
+            node.next = ListNode(i)
+            node = node.next
+
 
 class Solution:
+    # 空间复杂度为O(n)
     def palindromeList(self, head):
         """
         :type head: ListNode
@@ -23,3 +30,64 @@ class Solution:
                 return False
         return True
 
+    # 空间复杂度为O(1)
+    def palindromeList2(self, head):
+        if not head:
+            return True
+        if not head.next:
+            return True
+        count = 0
+        fast = head
+        slow = head
+        while head:
+            count += 1
+            head = head.next
+        l1 = count//2
+        l2 = count - l1
+        for i in range(l2):
+            fast = fast.next
+        f2 = fast.next
+        while f2:
+            f3 = f2.next
+            f2.next = fast
+            fast = f2
+            f2 = f3
+        for i in range(l1):
+            if fast.val != slow.val:
+                return False
+            fast = fast.next
+            slow = slow.next
+        return True
+
+    def palindromeList3(self, head):
+        if head == None or head.next == None:
+            return True
+
+        lat = head.next
+        pre = head
+        while lat != None and lat.next != None:
+            lat = lat.next.next
+            pre = pre.next
+
+        cur = pre.next
+        pre.next = None
+        p = None
+        while cur != None:
+            q = cur.next
+            cur.next = p
+            p = cur
+            cur = q
+
+        while p != None and head != None:
+            if p.val != head.val:
+                return False
+            p = p.next
+            head = head.next
+
+        return True
+
+
+pl = ListNode(1)
+pl.addList(pl, [1, 2, 1])
+s = Solution()
+print(s.palindromeList2(pl))
