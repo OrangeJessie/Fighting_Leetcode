83ddea225e8177170d09bfb4fd76771f02d2e0c5
Jessie <976448731@qq.com>
2019-01-04 21:24:03 +0800

    Update isSymmetric.py

--- 初级算法/isSymmetric.py
+++ 初级算法/isSymmetric.py
@@ -5,7 +5,9 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
+    # 递归的方法
     def isSymmetric(self, root):
         """
         :type root: TreeNode
@@ -15,9 +17,9 @@ class Solution(object):
             return True
         left = root.left
         right = root.right
-        return self.readRight(left, right)
+        return self.subRead(left, right)
 
-    def readRight(self, left, right):
+    def subRead(self, left, right):
         if left == None and right == None:
             return True
         elif left == None and right != None:
@@ -26,7 +28,27 @@ class Solution(object):
             return False
         elif left.val != right.val:
             return False
-        a = self.readRight(left.left, right.right)
-        b = self.readRight(left.right, right.left)
+        a = self.subRead(left.left, right.right)
+        b = self.subRead(left.right, right.left)
         return a and b
 
+    # 迭代的方法 速度比递归要快一点
+    # 迭代的方式遍历整棵树
+    def isSymmetric2(self, root):
+        if not root:
+            return True
+        nodeList = [root.left, root.right]
+        while nodeList:
+            symmetricLeft = nodeList.pop(0)
+            symmetricRight = nodeList.pop(0)
+            if not symmetricLeft and not symmetricRight:
+                continue
+            if not symmetricLeft or not symmetricRight:
+                return False
+            if symmetricLeft.val != symmetricRight.val:
+                return False
+            nodeList.append(symmetricLeft.left)
+            nodeList.append(symmetricRight.right)
+            nodeList.append(symmetricLeft.right)
+            nodeList.append(symmetricRight.left)
+        return True
